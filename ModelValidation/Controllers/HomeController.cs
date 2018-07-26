0faea8c9f34f81d2ac34b901e37ecba96c087aff
AndriiTomashevsky <tomashevskiy.a@gmail.com>
2018-07-26 19:31:50 +0300

    Add project files.

---
+++ ModelValidation/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using ModelValidation.Models;
+namespace ModelValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("MakeBooking", new Appointment { Date = DateTime.Now });
+        }
+
+        [HttpPost]
+        public ViewResult MakeBooking(Appointment appt)
+        {
+            return View("Completed", appt);
+        }
+    }
+}
