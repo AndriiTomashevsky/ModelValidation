94df90ac7f69c3017e1d49b832fe6c314fa06380
AndriiTomashevsky <tomashevskiy.a@gmail.com>
2018-07-26 20:35:42 +0300

    Explicitly Validating a Model

--- ModelValidation/Controllers/HomeController.cs
+++ ModelValidation/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using ModelValidation.Models;
 namespace ModelValidation.Controllers
 {
@@ -13,7 +14,31 @@ namespace ModelValidation.Controllers
         [HttpPost]
         public ViewResult MakeBooking(Appointment appt)
         {
-            return View("Completed", appt);
+            if (string.IsNullOrEmpty(appt.ClientName))
+            {
+                ModelState.AddModelError(nameof(appt.ClientName), "Please enter your name");
+            }
+
+            ModelValidationState modelValidationState = ModelState.GetValidationState("Date");
+
+            if (ModelState.GetValidationState("Date") == ModelValidationState.Valid && DateTime.Now > appt.Date)
+            {
+                ModelState.AddModelError(nameof(appt.Date), "Please enter a date in the future");
+            }
+
+            if (!appt.TermsAccepted)
+            {
+                ModelState.AddModelError(nameof(appt.TermsAccepted), "You must accept the terms");
+            }
+
+            if (ModelState.IsValid)
+            {
+                return View("Completed", appt);
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
--- ModelValidation/Startup.cs
+++ ModelValidation/Startup.cs
@@ -11,16 +11,18 @@ namespace ModelValidation
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc().AddMvcOptions(opts => opts.ModelBindingMessageProvider
+                    .SetValueMustNotBeNullAccessor(value => "Please enter a value")
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseBrowserLink();
             app.UseStatusCodePages();
             app.UseDeveloperExceptionPage();
             app.UseStaticFiles();
+            app.UseBrowserLink();
             app.UseMvcWithDefaultRoute();
         }
     }
