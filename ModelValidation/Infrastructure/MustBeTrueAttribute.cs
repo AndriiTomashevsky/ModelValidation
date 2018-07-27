bec0a0f063b2438d1931dc05a1ef84342b1d9b32
AndriiTomashevsky <tomashevskiy.a@gmail.com>
2018-07-27 10:03:14 +0300

    Creating a Custom Property Validation Attribute

---
+++ ModelValidation/Infrastructure/MustBeTrueAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+namespace ModelValidation.Infrastructure
+{
+    public class MustBeTrueAttribute : Attribute, IModelValidator
+    {
+        public bool IsRequired
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public string ErrorMessage { get; set; } = "This value must be true";
+
+        public IEnumerable<ModelValidationResult> Validate(ModelValidationContext context)
+        {
+            bool? value = context.Model as bool?;
+
+            if (!value.HasValue || value.Value == false)
+            {
+                return new List<ModelValidationResult> { new ModelValidationResult("", ErrorMessage) };
+            }
+            else
+            {
+                return Enumerable.Empty<ModelValidationResult>();
+            }
+        }
+    }
+}
